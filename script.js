40b5a810aaf4c3a6cdc35e34392ee46d977da9de
Cody Crawford <Cody.Crawford1010@gmail.com>
2022-02-16 10:57:29 -0600

    Completed Exercise

--- script.js
+++ script.js
@@ -27,3 +27,34 @@ console.log(randomNumber);
 
 randomNumber++;
 console.log(randomNumber);
+
+// BONUS
+// 6
+console.log(" Let It Go!".toUpperCase().repeat(2).trim()); // LET IT GO! LET IT GO!
+
+// 7a
+let reindeers = "Reindeers are better than people.";
+
+// 7b
+console.log(reindeers.replace(/ /g, "_"));
+
+// 8
+console.log(Math.sqrt(2)); // 1.4142135623730951
+// console.log(Math.SQRT2);
+
+// 9
+// Completed on separate lines
+// let newRandomNumber = Math.random();
+// newRandomNumber *= 17;
+// newRandomNumber = Math.floor(newRandomNumber);
+// newRandomNumber += 7;
+// console.log(newRandomNumber); // 7, 8, 9, up to 23
+
+// Extra Bonus" Completed ona single line 
+let newRandomNumber = Math.floor(Math.random() * (23 - 7 + 1) + 7);
+// let newRandomNumber = Math.floor(Math.random() * (16 + 1) + 7);
+// let newRandomNumber = Math.floor(Math.random() * 17 + 7);
+// let newRandomNumber = Math.floor(A number from 0 up to 16.999 + 7);
+// let newRandomNumber = Math.floor(A number from 7 up to 23.999);
+// let newRandomNumber = A whole number from 7 up to 23;
+console.log(newRandomNumber); // 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
